5341a58bd26a81ea7cf8041e8025d32879a7bc6e
Santiago Mendoza Ramirez <contacto@santiagomendoza.org>
2014-12-22 09:22:07 -0500

    Agregado Menú lateral
    Agregado menú lateral en página de inicio. Ordenar un poco los js, para
    dividirlos como "controladores" para cada página.

--- js/maps.js
+++ js/maps.js
@@ -1,8 +1,19 @@
 function initialize() {
+	var center = new google.maps.LatLng(4.397, 0.644) 
 	var mapOptions = {
-	  center: new google.maps.LatLng(4.397, 0.644),
+	  center: center,
 	  zoom: 4,
+	  streetViewControl: false,
+	  scaleControl: true,
 	  mapTypeId: google.maps.MapTypeId.TERRAIN
 	};
 	var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+
+	var infowindow = new google.maps.InfoWindow({
+    content: 'Change the zoom level',
+    position: center
+  	});
+
+  	infowindow.open(map);
+  	google.maps.event.addDomListener(window, 'load', initialize);
 }
