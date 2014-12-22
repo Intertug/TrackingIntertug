266c692c856da18290ecc12b6872470c1c4f79da
Santiago Mendoza Ramirez <contacto@santiagomendoza.org>
2014-12-22 11:26:18 -0500

    Agregada página de información del barco
    Agregada página shipinfo.html donde se puede encontrar la información
    completa del barco. Se coloco un menú fixed (fijo a la izquierda), para
    mantener el mismo diseño de la página del mapa y que sea mas entendible
    por los usuarios.
    
    Se coloco un prototipo visual de la página. Luego toca alimentarla con
    el webservice.

---
+++ js/ShipController/RequestShip.js
@@ -0,0 +1,15 @@
+function shipInfoRequest() {
+	xmlhttp = new XMLHttpRequest();
+	try{
+		xmlhttp.open("GET", "../views/parts.xml", false);
+		xmlhttp.send();
+	}catch(err){
+		document.getElementById("showrequest").innerHTML = err;
+	}
+	xmlDoc = xmlhttp.responseXML;
+	if(xmlDoc != null){
+		document.getElementById("showrequest").innerHTML = xmlDoc;
+	}else{
+		document.getElementById("showrequest").innerHTML = "Nulo";
+	}
+}
