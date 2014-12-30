c2a57df3adf1a20081a8735707e4f08cf88d195b
Santiago Mendoza Ramirez <contacto@santiagomendoza.org>
2014-12-30 15:42:10 -0500

    Rutas y template de AngularJS
    Se modificaron y arreglaron las rutas. Se modifico el index.html, donde
    se cargan todos los scripts y estilos. Además, se dejó un solo div,
    donde se inyectarán las demás vistas. En las otras vistas, solo está la
    parte visual, dejando al index el trabajo de cargar los scripts y
    estilos (con la posibilidad de que, si es necesario, se puedan cargar
    nuevos en la vista que se inyectará).

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,4 @@
+/*  Aqui configuramos la carga asincrona de google maps    */
 angular.module('uiGmapgoogle-maps').config(function(uiGmapGoogleMapApiProvider) {
     uiGmapGoogleMapApiProvider.configure({
         key: 'AIzaSyBEuaoh_rW9edjj3Ona1XpY2yOk090mJ5o',
@@ -6,17 +7,24 @@ angular.module('uiGmapgoogle-maps').config(function(uiGmapGoogleMapApiProvider) 
     });
 });
 
-var app = angular.module('app', ['uiGmapgoogle-maps', 'home', 'ngRoute']);
+/*   Aqui creamos el modulo principal de la aplicación y
+    configuramos las rutas. Además, agregamos aquí los controladores
+    que pueden ser comunes para toda la aplicación. Por ejemplo, la
+    creación del mapa.   */
+var app = angular.module('app', 
+    ['uiGmapgoogle-maps', 'home', 'ngRoute']);
 
 app.config(function($routeProvider, $locationProvider){
     $routeProvider.when("/", {
-        templateUrl : "/TrackingIntertug/index.html",
-        controller : "HomeController"
+        redirectTo: '/map'
     })
-    .when('/vesselinfo', {
-    	templateUrl: "/TrackingIntertug/views/shipinfo.html"
+    .when('/map', {
+    	templateUrl: "/TrackingIntertug/views/index.html"
     })
-    .otherwise({ reditrectTo : "/TrackingIntertug/views/shipinfo.html" });
+    .when('/info', {
+        templateUrl: "/TrackingIntertug/views/shipinfo.html"
+    })
+    .otherwise({ reditrectTo : "/" });
 
-    $locationProvider.html5Mode(true).hashPrefix('!');
+//    $locationProvider.html5Mode(true);//.hashPrefix('!');
 });
--- scripts/home/Controllers.js
+++ scripts/home/Controllers.js
@@ -7,7 +7,6 @@ homecontrollers.controller('HomeController',
     // The "then" callback function provides the google.maps object.
     uiGmapGoogleMapApi.then(function(maps) {
     	$scope.map = {center: {latitude: 10.397100876463162, longitude: -75.47116564868168 }, zoom: 14 };
-    	
     	$scope.marker = {
 	      id: 0,
 	      coords: {latitude: 10.397100876463162, longitude: -75.47116564868168 },
@@ -18,7 +17,7 @@ homecontrollers.controller('HomeController',
 }]);
 
 homecontrollers.controller('PeticionController', ['$scope', '$http', function($scope, $http){
-    url= 'http://ing-sis.jairoesc.com/vehicle?auth-token=eyJpdiI6IndTUGNJdFZLN0pKblJ4V0QwVVlzcGc9PSIsInZhbHVlIjoib0FtVmVSWXg4TzMyd2xheDQ2bDdzMGZkME5CQ3kraWEzOE5SN2FiTGtkcz0iLCJtYWMiOiIwNzU1MzAyNmRkZGJmZDRiNzNmZWU1M2ZmZmQzNDAwYjBhNzNmMjQ4ZDA2MTJmMWRhYzIzYzI4ZWFhY2VmNTMzIn0=';
+    url= 'http://ing-sis.jairoesc.com/vehicle?auth-token=eyJpdiI6IjVaemhPamdMcnZ3SXU5RHlmbTFTcUE9PSIsInZhbHVlIjoiZmpDdTVaRHA4UisyNjFTUmd3RVF4a2txakZiQjlEQlZ2SkpQNFpzZjY5cz0iLCJtYWMiOiJjNGVjZjE5ZWM4MTE2ZDJkOGMwMzkwMmQ5MTlkMjBiYjYyMTgyOTk1MTc2NDIzZWZlNjRlMDgwN2VmNmRkZjQwIn0=';
     $http.get(url)
     .success(function (data, status, headers, config) {
 		$scope.data=data;
