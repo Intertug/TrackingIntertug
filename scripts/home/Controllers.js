00995090626fa689a63c352c259b1fa501200b9e
Santiago Mendoza Ramirez <contacto@santiagomendoza.org>
2015-01-08 11:18:31 -0500

    Peticion al WebService y posicionar vessels
    Se realiza la petición al Web Service de intertug, del cual recibimos
    los vessels con toda su información. Actualmente se toman esos vessels y
    se posicionan en el mapa.

--- scripts/home/Controllers.js
+++ scripts/home/Controllers.js
@@ -1,71 +1,52 @@
 var homecontrollers = angular.module('home.controllers', ['ngResource']);
 
 homecontrollers.controller('HomeController',
-        ['$scope', 'uiGmapGoogleMapApi', '$log', function ($scope, uiGmapGoogleMapApi, $log) {
+        ['$scope', 'uiGmapGoogleMapApi', '$log', 'getVesselsPosition', function ($scope, uiGmapGoogleMapApi, $log, getVesselsPosition) {
                 // uiGmapGoogleMapApi is a promise.
                 // The "then" callback function provides the google.maps object.
-                uiGmapGoogleMapApi.then(function (maps) {
-                    $scope.map = {
-                        center: {
-                            latitude: 9.024365,
-                            longitude: -72.913396
-                        },
-                        zoom: 4
-                    };
-                    $scope.options = {
-                        scrollwheel: false
-                    };
-                    var markers = [];
-                    var tugs = [
-                        ["BaruPacifico", -3.58931333333, -80.746989, 4],
-                        ["Mistral", -3.58368333333, -80.7329345, 4],
-                        ["Vali", -3.50912833333, -80.660905, 4],
-                        ["BaruInti", -5.070405, -81.1084553333, 4],
-                        ["Boreas", 10.0004283333, -83.0791666667, 1],
-                        ["EosII", 10.3993143333, -75.5239095, 2],
-                        ["Alisios", 3.89337616667, -77.0774251667, 3],
-                        ["Capidahl", 9.48654383333, -75.7759115, 4],
-                        ["Saga", 10.4067566667, -75.5408853333, 4],
-                        ["Titania", 10.9744588333, -74.7546875, 4],
-                        ["Chinook", 11.084963, -74.2497558333, 4],
-                        ["Sirocco", 10.4049051667, -75.532373, 4],
-                        ["Aquavit", 9.48603316667, -75.774528, 4],
-                        ["Carex", 9.48936966667, -75.7729736667, 4],
-                        ["Tanok", 18.43208, -93.1872396667, 4],
-                        ["Seatrout", 18.4311401667, -93.2091633333, 4],
-                        ["Kin", 18.4321655, -93.187028, 4]
-                    ];
-                    for (i = 0; i < tugs.length; i++) {
-                        var ret = {
-                            latitude: tugs[i][1],
-                            longitude: tugs[i][2],
-                            title: 'm' + tugs[i][0],
-                            id: i + 1,
-                            icon: '/TrackingIntertug/imgs/ship.png'
-                        };
-                        markers.push(ret);
-                    }
-                    $scope.randomMarkers = markers;
-                    $scope.ClusterMarkersOptions = {};
-                    //$scope.ClusterMarkersOptions.averageCenter = {latitude: 40.1451,longitude: -99.6680}
-                    $scope.ClusterMarkersOptions.gridSize = 60;
-                });
-            }]);
 
-homecontrollers.controller('PeticionController', ['$scope', 'RequestService', '$http', function ($scope, RequestService, $http) {
-        url = 'http://ing-sis.jairoesc.com/vehicle?auth-token=eyJpdiI6IlVFajh0a1ArNnhMVWJSZWdFTmxmNUE9PSIsInZhbHVlIjoibEJ0WmMrZlRIMDUzMWpxQjFGOUtxNFlHUEFaWGNtY0Q1RnhObGhIaUh1WT0iLCJtYWMiOiJmNWMxNzcxZWZlZGNhOTU4YWE0NTU5ZjI1NmE0ODM2OTk2ZDhhODRjNDcyNzRmODAxMTc3ZDcxNTlmOWQ0ZDMxIn0=';
-        RequestService.get(url).then(
-                function (data) {
-                    console.log(data['vehicle'][0].brand);
-                    $scope.data = data;
-                },
-                function (error) {
-                    console.log(error);
-                    $scope.error = error;
-                }
-        );
-        //this.data = RequestService.get();
-    }]);
-/*var tugs = [
- ];
- */
+                $scope.initializeMap = function () {
+                    uiGmapGoogleMapApi.then(function (maps) {
+                        $scope.map = {
+                            center: {
+                                latitude: 9.024365,
+                                longitude: -72.913396
+                            },
+                            zoom: 4
+                        };
+                        $scope.options = {
+                            scrollwheel: false
+                        };
+                        var markers = [];
+                        for (i = 0; i < $scope.vessels.length; i++) {
+                            var ret = {
+                                latitude: $scope.vessels[i].lat,
+                                longitude: $scope.vessels[i].long,
+                                title: $scope.vessels[i].vesselname,
+                                id: $scope.vessels[i].id,
+                                icon: '/TrackingIntertug/imgs/ship.png'
+                            };
+                            markers.push(ret);
+                        }
+                        $scope.randomMarkers = markers;
+                        $scope.ClusterMarkersOptions = {};
+                        $scope.ClusterMarkersOptions.gridSize = 60;
+                    });
+                };
+
+                $scope.getVessels = function () {
+                    url = 'http://190.242.119.122:82/sioservices/daqonboardservice.asmx/GetVesselsPosition';
+                    getVesselsPosition.get(url).then(
+                            function (data) {
+                                var json = $.parseJSON(data.d);
+                                $scope.vessels = json.vessels.vessel;
+                                $scope.initializeMap();
+                            },
+                            function (error) {
+                                console.log(error.data);
+                                $scope.error = error;
+                            }
+                    );
+                };
+                $scope.getVessels();
+            }]);
--- scripts/home/Services.js
+++ scripts/home/Services.js
@@ -1,10 +1,17 @@
 var homeservices = angular.module('home.services', []);
-
-homeservices.factory("RequestService", ['$resource', '$http', function ($resource, $http) {
+homeservices.factory("getVesselsPosition", ['$resource', '$http', function ($resource, $http) {
         //url = 'http://ing-sis.jairoesc.com/vehicle?auth-token=eyJpdiI6IlVFajh0a1ArNnhMVWJSZWdFTmxmNUE9PSIsInZhbHVlIjoibEJ0WmMrZlRIMDUzMWpxQjFGOUtxNFlHUEFaWGNtY0Q1RnhObGhIaUh1WT0iLCJtYWMiOiJmNWMxNzcxZWZlZGNhOTU4YWE0NTU5ZjI1NmE0ODM2OTk2ZDhhODRjNDcyNzRmODAxMTc3ZDcxNTlmOWQ0ZDMxIn0=';
         return {
-            get: function(url){
-                var promise = $http.get(url).then(function(response){
+            get: function (url) {
+                var promise = $http({
+                    url: url,
+                    data: {GetData: '', SessionID: ''},
+                    method: 'POST',
+                    headers: {
+                        //'Content-Type': 'application/json'
+                        //'SOAPAction': 'http://com.intertug.sio/GetVesselsPosition'
+                    }
+                }).then(function (response) {
                     return response.data;
                 });
                 return promise;
