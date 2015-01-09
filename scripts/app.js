d59906b54f7eb94bb40ffafb8f4957e5d4d6326a
Ruben Melo <rbmcar2@gmail.com>
2015-01-09 11:59:39 -0500

    cambio en librerías y nudos

--- scripts/app.js
+++ scripts/app.js
@@ -3,7 +3,7 @@ angular.module('uiGmapgoogle-maps').config(function(uiGmapGoogleMapApiProvider) 
     uiGmapGoogleMapApiProvider.configure({
         key: 'AIzaSyBEuaoh_rW9edjj3Ona1XpY2yOk090mJ5o',
         v: '3.17',
-        libraries: 'weather,geometry,visualization'
+        libraries: 'visualization'
     });
 });
 
