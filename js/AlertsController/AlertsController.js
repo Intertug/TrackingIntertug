61d8c63e7c56193b09a9150462ceca14c096d8cd
Santiago Mendoza Ramirez <contacto@santiagomendoza.org>
2015-02-13 15:47:35 -0500

    No mostrar rpm para vessels que no lo tengan

--- js/AlertsController/AlertsController.js
+++ js/AlertsController/AlertsController.js
@@ -14,3 +14,35 @@ function alertas(vessels, mapa) {
 }
 
 
+Handlebars.registerHelper("alertafecha", function (gpsdate) {
+    var fecha = new Date(gpsdate);
+    var actual = new Date();
+    var alerta = "<a class='list-group-item-danger'>" + gpsdate.fn(this) + "</a>";
+    var alerta2 = "<a class=''>" + gpsdate.fn(this) + "</a>";
+    if (fecha.getFullYear() < actual.getFullYear()) {
+        return new Handlebars.SafeString(alerta);
+    } else if (fecha.getMonth() < actual.getMonth()) {
+        return new Handlebars.SafeString(alerta);;
+    } else if (fecha.getDay() < actual.getDay()) {
+        return new Handlebars.SafeString(alerta);;
+    } else if ((parseInt(actual.getHours()) - parseInt(fecha.getHours())) > 1) {
+        return new Handlebars.SafeString(alerta);;
+    } else if ((parseInt(actual.getHours()) - parseInt(fecha.getHours())) == 1) {
+        if ((parseInt(actual.getMinutes()) + (60 - parseInt(fecha.getMinutes()))) > 59) {
+            return new Handlebars.SafeString(alerta);;
+        }
+    } else {
+        return new Handlebars.SafeString(alerta2);
+    }
+});
+
+Handlebars.registerHelper("alertafecha", function (vessel) {
+    if (vessel.id == 5) {
+        if (parseFloat(vessel.speed) > 8)
+            return true;
+    } else {
+        if (parseFloat(vessel.speed) > 9)
+            return true;
+    }
+    return false;
+});
