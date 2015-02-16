cb5a798b974bb6d603ec962377e932e21cb05853
Santiago Mendoza Ramirez <contacto@santiagomendoza.org>
2015-02-16 09:21:07 -0500

    Markers Cluster activado
    Se agregó el archivo markerclusterer.js para hacer clusters entre los
    markers que están muy cerca. Se creo un gridsize (tamaño de grilla) de
    60.

--- js/HomeController/HomeController.js
+++ js/HomeController/HomeController.js
@@ -1,5 +1,5 @@
 function initmap() {
-    var centro = new google.maps.LatLng( 9.024365, -72.913396);
+    var centro = new google.maps.LatLng(9.024365, -72.913396);
     var opciones = {
         zoom: 4,
         center: centro,
@@ -30,16 +30,20 @@ function initmap() {
 
 function setMarkers(vessels, mapa) {
     vessels = vessels.vessel;
+    var markers = [];
     for (i = 0; i < vessels.length; i++) {
         var position = new google.maps.LatLng(vessels[i].lat, vessels[i].long);
         var marcador = new google.maps.Marker({
             position: position,
-            map: mapa,
+            //map: mapa,
             icon: '../imgs/ship.png',
             draggable: false,
             title: vessels[i].vesselname,
             zIndex: 1,
             html: vessels[i].vesselname
         });
+        markers[i] = marcador;
     }
+    var clusterOptions = {gridSize: 60};
+    var mc = new MarkerClusterer(mapa, markers, clusterOptions);
 }
