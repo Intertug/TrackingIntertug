bb4326d9c063df1024446aa70627c46dc4561247
Santiago Mendoza Ramirez <contacto@santiagomendoza.org>
2015-02-16 10:07:23 -0500

    Agregados infowindows a los vessels
    Mostrar infowindows al hacer mouseover (el mouse encima del vessel), y
    ocultarlo con mouseout (quitar el mouse de encima del vessel).

--- js/HomeController/HomeController.js
+++ js/HomeController/HomeController.js
@@ -31,18 +31,33 @@ function initmap() {
 function setMarkers(vessels, mapa) {
     vessels = vessels.vessel;
     var markers = [];
+    var infowindow = null;
+    infowindow = new google.maps.InfoWindow({
+        content: ""
+    });
     for (i = 0; i < vessels.length; i++) {
+        var content = "<h6>" + vessels[i].vesselname + " </h6>"
+                + "<b> Velocidad </b>: " + vessels[i].speed + " Nudos<br>"
+                + "<b> Fecha </b>: " + vessels[i].gpsdate
+                + "<br><br>";
         var position = new google.maps.LatLng(vessels[i].lat, vessels[i].long);
         var marcador = new google.maps.Marker({
             position: position,
-            //map: mapa,
             icon: '../imgs/ship.png',
             draggable: false,
             title: vessels[i].vesselname,
             zIndex: 1,
-            html: vessels[i].vesselname
+            html: content
         });
         markers[i] = marcador;
+
+        google.maps.event.addListener(marcador, 'mouseover', function () {
+            infowindow.setContent(this.html);
+            infowindow.open(mapa, this);
+        });
+        google.maps.event.addListener(marcador, 'mouseout', function () {
+            infowindow.close();
+        });
     }
     var clusterOptions = {gridSize: 60};
     var mc = new MarkerClusterer(mapa, markers, clusterOptions);
