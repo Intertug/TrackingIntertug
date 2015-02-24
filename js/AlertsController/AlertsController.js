1e062a75b304c8cb96f68ff4fac5cdb86d2bfaf7
Santiago Mendoza Ramirez <contacto@santiagomendoza.org>
2015-02-24 15:02:10 -0500

    Refactoring AlertsController

--- js/AlertsController/AlertsController.js
+++ js/AlertsController/AlertsController.js
@@ -5,7 +5,6 @@ function initAlerts() {
         getVesselsRequest(compileHandlebar);
         setInterval(getVesselsRequest, 60000, compileHandlebar);
     } catch (err) {
-        console.log(err);
         throw err;
     }
 }
@@ -17,44 +16,35 @@ function compileHandlebar(vessels, mapa) {
 }
 
 
-Handlebars.registerHelper("alertafecha", function (date) {
-    var fecha = new Date(date), actual = new Date(), bool = false;
+Handlebars.registerHelper("isOutdatedDate", function (date) {
+    var fecha = new Date(date), actual = new Date();
     if (fecha.getFullYear() < actual.getFullYear()) {
-        bool = true;
+        return "list-group-item-danger";
     } else if (fecha.getMonth() < actual.getMonth()) {
-        bool = true;
+        return "list-group-item-danger";
     } else if (actual.getDate() - fecha.getDate() > 1) {
-        bool = true;
+        return "list-group-item-danger";
     } else if (actual.getDate() - fecha.getDate() == 1) {
-        bool = true;
+        return "list-group-item-danger";
         if (parseInt(actual.getHours()) == 0 && parseInt(fecha.getHours()) == 23) {
-            bool = false;
+            return "";
         }
     } else if ((parseInt(actual.getHours()) - parseInt(fecha.getHours())) > 1) {
-        bool = true;
+        return "list-group-item-danger";
     } else if ((parseInt(actual.getHours()) - parseInt(fecha.getHours())) == 1) {
         if ((parseInt(actual.getMinutes()) + (60 - parseInt(fecha.getMinutes()))) > 59) {
-            bool = true;
+            return "list-group-item-danger";
         }
     } else {
-        bool = false;
-    }
-    if (bool) {
-        return "list-group-item-danger";
+        return "";
     }
 });
 
-Handlebars.registerHelper("alertavelocidad", function (vessel) {
-    var bool = false;
+Handlebars.registerHelper("isOverSpeedTop", function (vessel) {
     if (vessel.id == 5) {
         if (parseFloat(vessel.speed) > 8)
-            bool = true;
+            return "list-group-item-danger";
     } else if (parseFloat(vessel.speed) > 9) {
-        bool = true;
-    } else {
-        bool = false;
-    }
-    if (bool) {
         return "list-group-item-danger";
     } else {
         return "";
