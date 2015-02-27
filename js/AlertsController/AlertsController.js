059c2a18a016cda812a6e67dc3b4c3ee46249727
Santiago Mendoza Ramirez <contacto@santiagomendoza.org>
2015-02-27 10:21:16 -0500

    Cambiar la forma de comparar las fechas
    Restar y convertir a minutos.

--- js/AlertsController/AlertsController.js
+++ js/AlertsController/AlertsController.js
@@ -21,31 +21,15 @@ Handlebars.registerHelper("isOutdatedDate", function (date) {
     var fecha1 = fechita[0].split('-');
     var fecha2 = fechita[1].split(':');
     var año = fecha1[0];
-    var mes = fecha1[1]-1;
+    var mes = fecha1[1] - 1;
     var dia = fecha1[2];
     var hora = fecha2[0];
     var minutos = fecha2[1];
     var segundos = fecha2[2];
     var fecha = new Date(año, mes, dia, hora, minutos, segundos), actual = new Date();
-    console.log("Fecha gps:" + fecha);
-    console.log("Fecha actual:" + actual);
-    if (fecha.getFullYear() < actual.getFullYear()) {
+    var diferenciaEnMinutos = Math.abs((actual - fecha) / 60000);
+    if (diferenciaEnMinutos >= 30) {
         return "list-group-item-danger";
-    } else if (fecha.getMonth() < actual.getMonth()) {
-        return "list-group-item-danger";
-    } else if (actual.getDate() - fecha.getDate() > 1) {
-        return "list-group-item-danger";
-    } else if (actual.getDate() - fecha.getDate() == 1) {
-        return "list-group-item-danger";
-        if (parseInt(actual.getHours()) == 0 && parseInt(fecha.getHours()) == 23) {
-            return "";
-        }
-    } else if ((parseInt(actual.getHours()) - parseInt(fecha.getHours())) > 1) {
-        return "list-group-item-danger";
-    } else if ((parseInt(actual.getHours()) - parseInt(fecha.getHours())) == 1) {
-        if ((parseInt(actual.getMinutes()) + (60 - parseInt(fecha.getMinutes()))) > 59) {
-            return "list-group-item-danger";
-        }
     } else {
         return "";
     }
