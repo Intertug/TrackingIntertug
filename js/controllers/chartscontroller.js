2cdf516d9d12b73e598c12376714608cc2ccf95d
Santiago Mendoza Ramirez <contacto@santiagomendoza.org>
2015-04-29 08:23:46 -0500

    Update de las vistas al nuevo diseño.
    Update de las vistas al nuevo diseño.

--- js/controllers/chartscontroller.js
+++ js/controllers/chartscontroller.js
@@ -1,26 +1,54 @@
-function initCharts() {
-    getVesselsPosition(drawChart);
-    setInterval(getVesselsPosition, 60000, drawAddChart);
-}
-
-function drawChart(vessels) {
-    try {
-        for (i = 0; i < vessels.vessel.length; i++) {
-            var chart = new google.visualization.ColumnChart(document.getElementById('chart_div'));
-            var data = new google.visualization.DataTable();
-            data.addColumn('string', 'Vessel');
-            data.addColumn('number', 'RPM');
-            data.addRow([
-                vessels.vessel[i].vesselname,
-                parseFloat(vessels.vessel[i].rpm),
-            ]);
-            chart.draw(data);
-        }
-    } catch (e) {
-        throw e;
+var request = {
+    getVesselsPosition: function () {
+        getVesselsPosition(controller.setModel);
     }
-}
+};
 
-function drawAddChart(vessels){
-    
+var model = {
+    vessels: {},
+    options: {},
+    setVessels: function (vessels) {
+        this.vessels = vessels;
+    },
+    setOptions: function (options) {
+        this.options = options;
+    }
+};
+
+var controller = {
+    setModel: function (datos) {
+        model.setVessels(datos.vessels);
+        model.setOptions(datos.options);
+        controller.setChart(model.vessels);
+    },
+    setChart: function (vessels) {
+        views.renderChart(vessels);
+    }
+};
+
+var views = {
+    renderChart: function (vessels) {
+        try {
+            var vessels = vessels;
+            var chart = new google.visualization.ColumnChart(document.getElementById('chart_div'));
+            for (var i = 0, len = vessels.vessel.length; i < len; i++) {
+                var data = new google.visualization.DataTable();
+                data.addColumn('string', 'Vessel');
+                data.addColumn('number', 'RPM');
+                data.addRow([
+                    vessels.vessel[i].vesselname,
+                    parseFloat(vessels.vessel[i].rpm)
+                ]);
+                chart.draw(data);
+            }
+        } catch (e) {
+            console.log(e);
+            throw e;
+        }
+    }
+};
+
+function initialize() {
+    request.getVesselsPosition();
+    setInterval(request.getVesselsPosition, 60000);
 }
